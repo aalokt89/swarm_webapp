ecad6d330cd640e17dec92769253c0c1575c61ca
Aalok Trivedi <aaloktrivedi89@gmail.com>
2023-03-08 20:54:39 -0500

    replace redis with node

--- scripts/user_data.sh
+++ scripts/user_data.sh
@@ -3,6 +3,7 @@ sudo yum update -y
 sudo yum install -y docker
 sudo curl -L https://github.com/docker/compose/releases/latest/download/docker-compose-$(uname -s)-$(uname -m) -o /usr/local/bin/docker-compose
 sudo chmod +x /usr/local/bin/docker-compose
+sudo systemctl enable docker
 sudo systemctl start docker
 
 
